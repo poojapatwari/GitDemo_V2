322108b00cb4d251f639a8bab58d494a663ece4a
Ajinkya <Ajinkya@DESKTOP-38JQG8D>
2023-07-28 16:00:04 +0530

    First commit

---
+++ src/git/First_Program.java
@@ -0,0 +1,12 @@
+package git;
+
+public class First_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my First Program");
+
+	}
+
+}
